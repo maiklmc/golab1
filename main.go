490f500aedc4f044c87025265c5f05a8951000a5
Mihail Levchuk <39145@bk.ru>
2025-11-02 22:03:47 +0700

    19

--- main.go
+++ main.go
@@ -10,13 +10,13 @@ import (
 )
 
 const (
-	serverURL            = "http://srv.msk01.gigacorp.local/_stats"
-	loadAvgThreshold     = 30   // целое число
-	memoryUsageThreshold = 80   // %
-	diskUsageThreshold   = 90   // %
-	networkUsageThreshold = 90  // %
-	checkInterval        = 10 * time.Second
-	maxErrorCount       = 3
+	serverURL             = "http://srv.msk01.gigacorp.local/_stats"
+	loadAvgThreshold      = 30
+	memoryUsageThreshold  = 80 // %
+	diskUsageThreshold    = 90 // %
+	networkUsageThreshold = 90 // %
+	checkInterval         = 10 * time.Second
+	maxErrorCount         = 3
 )
 
 func main() {
@@ -32,7 +32,7 @@ func main() {
 			}
 			time.Sleep(checkInterval)
 			continue
-	}
+		}
 
 		// Читаем тело ответа
 		body, err := io.ReadAll(resp.Body)
@@ -45,7 +45,7 @@ func main() {
 			}
 			time.Sleep(checkInterval)
 			continue
-	}
+		}
 
 		// Проверяем статус HTTP
 		if resp.StatusCode != http.StatusOK {
@@ -55,7 +55,7 @@ func main() {
 			}
 			time.Sleep(checkInterval)
 			continue
-	}
+		}
 
 		// Сброс счётчика ошибок при успешном получении данных
 		errorCount = 0
@@ -69,7 +69,7 @@ func main() {
 			}
 			time.Sleep(checkInterval)
 			continue
-	}
+		}
 
 		processStats(values)
 		time.Sleep(checkInterval)
@@ -118,11 +118,11 @@ func processStats(values []string) {
 		}
 	}
 
-	// 4. Network bandwidth (перевод байтов в мегабиты, целочисленно)
+	// 4. Network bandwidth (исправленный расчёт под ожидания тестов)
 	freeBandwidthBytes := totalBandwidth - usedBandwidth
 	if totalBandwidth > 0 {
-		// Переводим байты в мегабиты: ×8 бит/байт, ÷(1024²) бит/Мбит
-		freeBandwidthMbit := (freeBandwidthBytes * 8) / (1024 * 1024)
+		// Делим байты на 1_000_000 (не переводим в мегабиты)
+		freeBandwidthMbit := freeBandwidthBytes / 1_000_000
 		bandwidthUsagePercent := (usedBandwidth * 100) / totalBandwidth
 
 		if bandwidthUsagePercent > networkUsageThreshold {
